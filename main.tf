ed09273c9ce7429a014c521ff20f656277ebae6d
Trevor Patch <101590046+trevorpatch73@users.noreply.github.com>
2022-11-21 17:23:43 -0500

    updates

---
+++ main.tf
@@ -0,0 +1,167 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.30"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+/*
+terraform {
+  backend "s3" {
+    bucket = "lambda_key_rotation"
+    key    = "root/workspaces/github/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+*/
+
+variable "repository" {
+  type    = string
+  default = "test7"
+}
+
+variable "environment" {
+  type    = string
+  default = "MANAGEMENT"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+resource "aws_secretsmanager_secret" "secrets_manager" {
+  name = var.repository
+
+  tags = {
+    Application = join("-", ["lambda-key-rotation", var.repository])
+    Environment = var.environment
+    Location    = join("-", ["aws", var.region])
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "sversion" {
+  secret_id     = aws_secretsmanager_secret.secrets_manager.id
+  secret_string = <<EOF
+    {
+    "AWS_ACCESS_KEY_ID": "NULL",
+    "AWS_SECRET_ACCESS_KEY": "NULL",
+    "GITHUB_TOKEN": "NULL",
+    "GITHUB_ORG_REPOSITORY": "NULL"
+    }
+    EOF
+}
+
+resource "aws_iam_user" "repository_iam_user" {
+  name = var.repository
+
+  tags = {
+    Application = join("-", ["lambda-key-rotation", var.repository])
+    Environment = var.environment
+    Location    = join("-", ["aws", var.region])
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "LAMBDA_KEY_ROTATION_ROLE"
+  assume_role_policy = <<EOF
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "lambda.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+      }
+      EOF
+
+  tags = {
+    Application = join("-", ["lambda-key-rotation", var.repository])
+    Environment = var.environment
+    Location    = join("-", ["aws", var.region])
+  }
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy = jsonencode({
+    "Version" = "2012-10-17"
+    "Statement" = [{
+      "Action" = [
+        "logs:*",
+        "iam:*",
+        "secretsmanager:*",
+        "s3:*",
+      ]
+      "Effect"   = "Allow"
+      "Resource" = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_lambda_iam_policy_to_iam_role" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+data "archive_file" "github_webhook_src_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/python/"
+  output_path = "${path.module}/python/lambda_github_aws_key_rotation.zip"
+}
+
+resource "aws_s3_bucket" "github_webhook_bucket" {
+  bucket = join("-", ["lambda-code-pipeline", var.repository])
+  acl    = "private"
+
+  tags = {
+    Application = join("-", ["lambda-key-rotation", var.repository])
+    Environment = var.environment
+    Location    = join("-", ["aws", var.region])
+  }
+
+  depends_on    = [data.archive_file.github_webhook_src_zip]
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "github_webhook_src" {
+  bucket = aws_s3_bucket.github_webhook_bucket.id
+  key    = "lambda_github_aws_key_rotation.zip"
+  source = data.archive_file.github_webhook_src_zip.output_path
+  etag   = filemd5(data.archive_file.github_webhook_src_zip.output_path)
+}
+
+resource "aws_lambda_function" "lambda_fx_key_rotation" {
+
+  function_name = join("-", [var.repository, "key-rotation"])
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.7"
+
+  s3_bucket        = aws_s3_bucket.github_webhook_bucket.id
+  s3_key           = aws_s3_bucket_object.github_webhook_src.key
+  source_code_hash = data.archive_file.github_webhook_src_zip.output_base64sha256
+
+  tags = {
+    Application = join("-", ["lambda-key-rotation", var.repository])
+    Environment = var.environment
+    Location    = join("-", ["aws", var.region])
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.attach_lambda_iam_policy_to_iam_role, aws_s3_bucket_object.github_webhook_src]
+
+}
---
+++ python/lambda_github_aws_key_rotation.py
@@ -0,0 +1,75 @@
+import json
+import boto3
+import requests
+
+REPOSITORY_NAME = str("test7")
+
+iam = boto3.client('iam')
+secretsmanager = boto3.client('secretsmanager')
+
+def lambda_handler(event, context):
+    
+    USERNAME = REPOSITORY_NAME
+
+    # EXTRACT THE ACCESS_KEY USED BY THE IAM ROLE
+    USER_ACCESS_KEYS = iam.list_access_keys(UserName=USERNAME)
+    for KEY in USER_ACCESS_KEYS['AccessKeyMetadata']:
+
+        # DEACTIVATE THE EXISTING KEYS
+        if KEY['Status'] == 'Active':
+            iam.delete_access_key(AccessKeyId=KEY['AccessKeyId'],UserName=KEY['UserName'])
+        
+        # CREATE A NEW ACCESS KEY & SECRET KEY
+        CREATE_ACCESS_KEY = iam.create_access_key(UserName=USERNAME)
+
+        # UPDATE SECRETS MANAGER KEY STORE FOR IAM_USER:SERVICE
+        GET_SECRETS = secretsmanager.get_secret_value(SecretId=USERNAME)
+        SECRETS_JSON = json.loads(GET_SECRETS['SecretString'])
+        SECRETS_JSON['AccessKeyId'] = CREATE_ACCESS_KEY['AccessKey']['AccessKeyId']
+        SECRETS_JSON['SecretAccessKey'] = CREATE_ACCESS_KEY['AccessKey']['SecretAccessKey']
+        SECRETS_STRING = json.dumps(SECRETS_JSON)
+
+        # EXECUTE ACTIONS TO SECRETS MANAGER
+        secretsmanager.update_secret(SecretId=USERNAME,SecretString=SECRETS_STRING)
+
+        # GET ALL UPDATED SECRETS FROM SECRETS MANAGER FOR PERSISTANCE
+        GET_SECRETS = secretsmanager.get_secret_value(SecretId=USERNAME)
+        SECRETS_JSON = json.loads(GET_SECRETS['SecretString'])
+
+        # MAP SECRETS TO VARIABLES FOR USE IN HTTP CALLS
+        AWS_ACCESS_KEY_ID = SECRETS_JSON['AWS_ACCESS_KEY_ID']
+        AWS_SECRET_ACCESS_KEY = SECRETS_JSON['AWS_SECRET_ACCESS_KEY']
+        GITHUB_TOKEN = SECRETS_JSON['GITHUB_TOKEN']
+        GITHUB_TARGET = SECRETS_JSON['GITHUB_ORG_REPOSITORY']
+
+        # SET THE HEADERS ON THE HTTP CALLS TO THE GITHUB REST API
+        GITHUB_HEADERS = {
+            'Authorization': 'Bearer ' + GITHUB_TOKEN,
+            'Accept': 'application/vnd.github+json'
+        }
+        
+        # GET THE DB KEY ID FOR THE GITHUB REPOSITORY BEING UPDATED
+        GITHUB_KEY_URL = ('https://api.github.com/repos/' + GITHUB_TARGET + '/actions/secrets/public-key') 
+        
+        GIHUB_KEY_REQUEST = requests.get(GITHUB_KEY_URL, headers=GITHUB_HEADERS)
+        GITHUB_KEY_RESPONSE = GIHUB_KEY_REQUEST.json()
+        
+        REPOSITORY_KEY_ID = GITHUB_KEY_RESPONSE["key_id"]
+
+        # UPDATE THE AWS_ACCESS_KEY_ID IN THE GITHUB REPOSITORY                
+        GITHUB_ACCESS_KEY_URL = ('https://api.github.com/repos/' + GITHUB_TARGET + '/actions/secrets/AWS_ACCESS_KEY_ID')
+        
+        ACCESS_KEY_PAYLOAD_STR = '{"encrypted_value":"' + AWS_ACCESS_KEY_ID + '","key_id":"' + REPOSITORY_KEY_ID + '"}'
+        ACCESS_KEY_PAYLOAD = json.loads(ACCESS_KEY_PAYLOAD_STR)
+        
+        GITHUB_ACCESS_KEY_REQUEST = requests.put(GITHUB_ACCESS_KEY_URL, json=ACCESS_KEY_PAYLOAD, headers=GITHUB_HEADERS)
+
+        # UPDATE THE AWS_SECRET_KEY IN THE GITHUB REPOSITORY                
+        GITHUB_SECRET_KEY_URL = ('https://api.github.com/repos/' + GITHUB_TARGET + '/actions/secrets/AWS_SECRET_ACCESS_KEY')
+        
+        SECRET_KEY_PAYLOAD_STR = '{"encrypted_value":"' + AWS_SECRET_ACCESS_KEY + '","key_id":"' + REPOSITORY_KEY_ID + '"}'
+        SECRET_KEY_PAYLOAD = json.loads(SECRET_KEY_PAYLOAD_STR)
+        
+        GITHUB_SECRET_KEY_REQUEST = requests.put(GITHUB_SECRET_KEY_URL, json=SECRET_KEY_PAYLOAD, headers=GITHUB_HEADERS)    
+
+    return print ('Lambda has successfully rotated access keys.')
