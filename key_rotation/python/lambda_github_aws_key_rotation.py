2ee2467ae994a6f57788d41c95cdda8455c196be
Trevor Patch <101590046+trevorpatch73@users.noreply.github.com>
2022-12-02 21:04:03 -0500

    destroy

--- key_rotation/python/lambda_github_aws_key_rotation.py
+++ key_rotation/python/lambda_github_aws_key_rotation.py
@@ -2,7 +2,7 @@ import json
 import boto3
 import requests
 
-REPOSITORY_NAME = str("test8")
+REPOSITORY_NAME = str("INSERT_GITHUB_REPOSITORY_NAME")
 
 iam = boto3.client('iam')
 secretsmanager = boto3.client('secretsmanager')
